a7197c2763f802aa90fcfd844069c7434f94e2db
xASonicMenx <78750060+xASonicMenx@users.noreply.github.com>
2021-03-09 12:36:42 -0600

    Update Main.py
    it may change over time.

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
-Null
+print ("[BOT] Mechanism: Hola")
+ans = input()
+print ("[BOT] Mechanism: Cual es tu nombre?")
+name = input()
+print = ("[BOT] Mechanism: Oh, Genial Hola" + name + ".")
+ans = input ()
+print ("[BOT] Mechanism: Como estas?" + name +)
+asn = input()
+print ("[BOT] Mechanism: Estoy funcionando correctamente, gracias por preocuparte!.")
+ans = input()
+print  ("[BOT] Mechanism: increíble!.")
+
+
